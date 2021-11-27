e6f150b68abbb4714d9d94ef5b5f4060dd7d20a8
developerpreety <preetyverma5555@gmail.com>
2021-11-28 02:29:45 +0530

    RMS-123 This commit display a new message.

--- src/main/java/GitDemo.java
+++ src/main/java/GitDemo.java
@@ -2,5 +2,6 @@ public class GitDemo {
     public static void main(String[] args) {
         System.out.println("Hey repository");
         System.out.println("I'm new repository");
+        System.out.println("Hey are you there");
     }
 }
